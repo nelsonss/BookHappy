4fb810d9dfe3439e5d7f2fbc5312366b86d4a23d
nelsonss <nelson.sanchezs@gmail.com>
2020-12-20 10:15:13 -0500

    Introdución a R

---
+++ introductionToR.R
@@ -0,0 +1,9 @@
+# Estadística con aplicaciones en R. / Manuel Ricardo Contento Rubio
+# Datos antropométricos en jóvenes. Página 26
+Edad <- c(3,5,6,8,10,3,6,8,14)
+Peso <- c(14,17,19,23,32,12,20,26,40)
+mean(Peso)
+sd(Edad)
+cor(Edad, Peso)
+plot(Edad, Peso)
+data(BOD)
